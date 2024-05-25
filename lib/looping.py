5294598f50cfe24bd77225c77af9b6c127d3fad6
Abdulhakimkullow <abdulhakim.kullow@student.moringaschool.com>
2024-05-25 10:15:14 +0300

    completed lab

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,30 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    for i in range(10, 0, -1):
+        print(i)
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+   # code goes here!
+    squared_integers = list()
+    for i in int_list:
+        squared_value = i ** 2  
+        squared_integers.append(squared_value)  
+    return squared_integers
+      
+    
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+ 
